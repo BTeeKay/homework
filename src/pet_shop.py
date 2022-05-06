3bf1b668506f7163658774062fae9cd1487232a9
Brian Kerr <briantkerr91@gmail.com>
2022-05-06 15:28:17 +0100

    Think I have finished

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -52,4 +52,40 @@ def remove_pet_by_name(list, name):
     petts = find_pet_by_name(list, name)
     list["pets"].remove(petts)
 
+def add_pet_to_stock(shop, pet):
+    shop["pets"].append(pet)
 
+def get_customer_cash(bob):
+    return bob["cash"]
+
+def remove_customer_cash(cust, cash):
+    cust["cash"] -= cash
+
+def get_customer_pet_count(cust):
+    return len(cust["pets"])
+
+def add_pet_to_customer(cust, pet):
+    cust["pets"].append(pet)
+
+def customer_can_afford_pet(cust, pet):
+    
+    afford = False
+
+    if cust["cash"] >= pet["price"]:
+        afford = True
+
+    return afford
+
+
+def sell_pet_to_customer(list, pet, cust):
+    # check customer has enough, remove pet from store, remove cash and add pet to customer
+    # add money to store
+    
+    if pet != None:
+        check = False
+        check = customer_can_afford_pet(cust, pet)
+        if check == True:
+            add_pet_to_customer(cust, pet)
+            increase_pets_sold(list, 1)
+            remove_customer_cash(cust, pet["price"])
+            add_or_remove_cash(list, pet["price"])
